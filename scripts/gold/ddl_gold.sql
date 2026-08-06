51361bb38a941f5392b33f45932ed4aa53c9a50a
Melat Teshome <51263172+melatteshome@users.noreply.github.com>
2026-08-06 22:13:27 +0300

    Refactor SQL views for customers, products, and sales

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -1,60 +1,56 @@
-create view Gold.dim_customers as
-select 
-  row_number() over (order by cst_id) as customer_key,
-  ci.cst_id as cusomer_id,
-  ci.cst_key as cusomer_number,
-  ci.cst_firstname as first_name,
-  ci.cst_lastname as last_name,
-  ci.cst_marital_status as martial_status ,
-case when ci.cst_gndr != 'n/a' then ci.cst_gndr
-	  else coalesce(ca.gen, 'n/a')
-    end as gender,
-  ci.cst_create_date as created_date,
-  ci.dwh_create_date ,
-  ca.bdate as birth_date,
-  la.cntry as country
- from 
-    Silver.crm_cust_info ci
- left join
-    Silver.erp_cust_az12 ca
- on ci.cst_key = ca.cid
- left join 
-    Silver.erp_loc_a101 la
- on ci.cst_key = la.cid;
+CREATE VIEW Gold.dim_customers AS
+SELECT 
+    ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key,
+    ci.cst_id AS cusomer_id,
+    ci.cst_key AS cusomer_number,
+    ci.cst_firstname AS first_name,
+    ci.cst_lastname AS last_name,
+    ci.cst_marital_status AS martial_status,
+    CASE
+        WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr
+        ELSE COALESCE(ca.gen, 'n/a')
+    END AS gender,
+    ci.cst_create_date AS created_date,
+    ci.dwh_create_date,
+    ca.bdate AS birth_date,
+    la.cntry AS country
+FROM Silver.crm_cust_info ci
+LEFT JOIN Silver.erp_cust_az12 ca
+    ON ci.cst_key = ca.cid
+LEFT JOIN Silver.erp_loc_a101 la
+    ON ci.cst_key = la.cid;
 
 
-
-create view Gold.dim_products as 
-select 
-	row_number() over (order by pn.prd_start_dt , pn.prd_key) as Product_key,
-	pn.prd_id as product_id,
-    pn.cat_id as category_id,
-    pn.prd_key as product_number,
-    pn.prd_nm as product_name,
-    pn.prd_cost as product_cost,
-    pn.prd_line as product_line,
-    pn.prd_start_dt as start_date,
-    pc.cat as category,
-    pc.subcat as sub_category,
+CREATE VIEW Gold.dim_products AS
+SELECT 
+    ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key,
+    pn.prd_id AS product_id,
+    pn.cat_id AS category_id,
+    pn.prd_key AS product_number,
+    pn.prd_nm AS product_name,
+    pn.prd_cost AS product_cost,
+    pn.prd_line AS product_line,
+    pn.prd_start_dt AS start_date,
+    pc.cat AS category,
+    pc.subcat AS sub_category,
     pc.maintenance
- from Silver.crm_prd_info pn
- left join Silver.erp_px_cat_g1v2 pc
- on pn.cat_id = pc.id
- where prd_end_dt is null
-
+FROM Silver.crm_prd_info pn
+LEFT JOIN Silver.erp_px_cat_g1v2 pc
+    ON pn.cat_id = pc.id
+WHERE prd_end_dt IS NULL;
 
 
 CREATE VIEW Gold.fact_sales AS
 SELECT
-    sd.sls_ord_num  AS order_number,
-    pr.product_key  AS product_key,
+    sd.sls_ord_num AS order_number,
+    pr.product_key AS product_key,
     cu.customer_key AS customer_key,
     sd.sls_order_dt AS order_date,
-    sd.sls_ship_dt  AS shipping_date,
-    sd.sls_due_dt   AS due_date,
-    sd.sls_sales    AS sales_amount,
+    sd.sls_ship_dt AS shipping_date,
+    sd.sls_due_dt AS due_date,
+    sd.sls_sales AS sales_amount,
     sd.sls_quantity AS quantity,
-    sd.sls_price    AS price
+    sd.sls_price AS price
 FROM Silver.crm_sales_details sd
 LEFT JOIN Gold.dim_products pr
     ON sd.sls_prd_key = pr.product_number
